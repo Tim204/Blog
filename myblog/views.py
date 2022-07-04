8c511d66a7aa01ff3597a7019f46adacfb498a21
Timoteo Tati <timoteo14@live.com.pt>
2022-07-04 15:13:09 +0200

    Commented out class based list view

--- myblog/views.py
+++ myblog/views.py
@@ -16,7 +16,7 @@ def post_list(request, tag_slug=None):
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in=[tag])
 
-    paginator = Paginator(object_list, 3) # 3 posts in each page
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -26,24 +26,22 @@ def post_list(request, tag_slug=None):
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-        
+
     context = {'page': page,
                'posts': posts,
                'tag': tag
                }
     return render(request,
-                 'myblog/post/list.html',
-                 context,
-                 )
+                  'myblog/post/list.html',
+                  context,
+                  )
 
 
-
-
-class PostListView(ListView):
-    queryset = BlogPost.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'myblog/post/list.html'
+# class PostListView(ListView):
+#     queryset = BlogPost.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'myblog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -74,7 +72,7 @@ def post_detail(request, year, month, day, post):
     context = {'post': post,
                'comments': comments,
                'new_comment': new_comment,
-               'comment_form': comment_form, 
+               'comment_form': comment_form,
                }
     return render(request,
                   'myblog/post/detail.html',
