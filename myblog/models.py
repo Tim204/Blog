899e1cf2ab3c6eceffdc872ab2b8afe92ba9b353
Timoteo Tati <timoteo14@live.com.pt>
2022-07-04 10:18:36 +0200

    Modifyied BlogPost model to include "tags" field

--- myblog/models.py
+++ myblog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -30,6 +31,7 @@ class BlogPost(models.Model):
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+    tags = TaggableManager()
 
     class Meta:
         # Orders by date published from latest to earliest
