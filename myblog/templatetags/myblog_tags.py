2967950311022150e6e3065dbb2e5f73f416a230
Timoteo Tati <timoteo14@live.com.pt>
2022-07-07 21:12:08 +0200

    Created "latest_posts" template tag

--- myblog/templatetags/myblog_tags.py
+++ myblog/templatetags/myblog_tags.py
@@ -3,6 +3,14 @@ from ..models import BlogPost
 
 register = template.Library()
 
+
 @register.simple_tag
-def total_posts(): # Django uses the function'sname as the tag name
+def total_posts():  # Django uses the function'sname as the tag name
     return BlogPost.published.count()
+
+
+@register.inclusion_tag('myblog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = BlogPost.published.order_by('-publish')[:count]
+    context = {'latest_posts': latest_posts}
+    return context
